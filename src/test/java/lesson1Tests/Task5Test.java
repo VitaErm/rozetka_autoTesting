30853138674c66090ec823664ba3e028bff4c0fb
Vita Erm <vita.iermolenko@gmail.com>
2020-05-12 14:22:10 +0300

    homework

---
+++ src/test/java/lesson1Tests/Task5Test.java
@@ -0,0 +1,24 @@
+package test.java.lesson1Tests;
+
+import main.java.lesson1.Task5;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class Task5Test {
+    @Parameters({"name"})
+    @Test
+    public static void correctReverse(@Optional("QWERTY") String name) {
+        String actual = Task5.reverse(name);
+        assertEquals(actual, "spoooO", "Error!");
+    }
+
+    @Test
+    public static void correctReverse2(@Optional("QWERTY") String name) {
+        String actual = Task5.reverse(name);
+        assertEquals(actual, Task5.reverse("QWERTY"), "Error!");
+    }
+}
